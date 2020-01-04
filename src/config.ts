5597b4a4d77260d529066091e2d86f89a495bec2
James Brumond <james@jbrumond.me>
2020-01-03 19:08:03 -0800

    update config for user service

--- src/config.ts
+++ src/config.ts
@@ -35,25 +35,25 @@ export interface Config {
 
 export const config: Config = {
 	http: {
-		port: cast.number(process.env.auth_srv_http_port, 8080),
-		address: cast.string(process.env.auth_srv_http_addr, '0.0.0.0')
+		port: cast.number(process.env.user_srv_http_port, 8080),
+		address: cast.string(process.env.user_srv_http_addr, '0.0.0.0')
 	},
 
 	logging: {
-		colors: cast.bool(process.env.auth_srv_logging_color, false),
+		colors: cast.bool(process.env.user_srv_logging_color, false),
 		output: 'pretty',
-		logLevel: cast.string(process.env.auth_srv_logging_level, 'info'),
-		stackTraceLimit: cast.number(process.env.auth_srv_logging_stack_length, 10),
+		logLevel: cast.string(process.env.user_srv_logging_level, 'info'),
+		stackTraceLimit: cast.number(process.env.user_srv_logging_stack_length, 10),
 	},
 
 	database: {
 		master: {
 			host: cast.string(process.env.db_users_master_host),
 			port: cast.number(process.env.db_users_master_port),
-			user: cast.string(process.env.auth_srv_db_users_user),
-			password: cast.string(process.env.auth_srv_db_users_pass),
+			user: cast.string(process.env.user_srv_db_users_user),
+			password: cast.string(process.env.user_srv_db_users_pass),
 			database: cast.string(process.env.db_users_database),
-			connectionLimit: cast.number(process.env.auth_srv_db_users_master_connection_limit, 100),
+			connectionLimit: cast.number(process.env.user_srv_db_users_master_connection_limit, 100),
 			supportBigNumbers: true,
 			bigNumberStrings: true,
 			dateStrings: true
@@ -61,10 +61,10 @@ export const config: Config = {
 		replica: {
 			host: cast.string(process.env.db_users_replica_host),
 			port: cast.number(process.env.db_users_replica_port),
-			user: cast.string(process.env.auth_srv_db_users_user),
-			password: cast.string(process.env.auth_srv_db_users_pass),
+			user: cast.string(process.env.user_srv_db_users_user),
+			password: cast.string(process.env.user_srv_db_users_pass),
 			database: cast.string(process.env.db_users_database),
-			connectionLimit: cast.number(process.env.auth_srv_db_users_replica_connection_limit, 100),
+			connectionLimit: cast.number(process.env.user_srv_db_users_replica_connection_limit, 100),
 			supportBigNumbers: true,
 			bigNumberStrings: true,
 			dateStrings: true
@@ -72,7 +72,7 @@ export const config: Config = {
 	},
 
 	refTables: {
-		refreshInterval: cast.number(process.env.auth_srv_ref_data_refresh_interval)
+		refreshInterval: cast.number(process.env.user_srv_ref_data_refresh_interval)
 	},
 
 	services: {
